204a38abc2813c77307a53f56d625bce73b9b6b9
sonideepak08 <100990869+sonideepak08@users.noreply.github.com>
2026-05-30 23:30:29 +0530

    refactoring code

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,14 +5,17 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      res.status(401).send("Please login!");
+      return res.status(401).send("Please login!");
     }
     const decode = await jwt.verify(token, "Deepakbhaiya@123");
     const userData = await User.findById(decode.userId);
+    if (!userData) {
+      return res.status(404).send("User not found");
+    }
     req.user = userData;
     next();
   } catch (error) {
-    res.status(400).send("ERROR: " + error.message);
+    return res.status(400).send("ERROR: " + error.message);
   }
 };
 
